a4e60e5b8f551dd85e32507fced8225e8e765222
Hamza Daqa <hamza.daqa@nibw.es>
2020-11-26 19:38:50 +0100

    whatsapp

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAIysA30EpDPz08Pf-axCaHuLh4A8uS2nw",
+//     authDomain: "whatsapp-mern-5665a.firebaseapp.com",
+//     databaseURL: "https://whatsapp-mern-5665a.firebaseio.com",
+//     projectId: "whatsapp-mern-5665a",
+//     storageBucket: "whatsapp-mern-5665a.appspot.com",
+//     messagingSenderId: "1073128270967",
+//     appId: "1:1073128270967:web:7a51664a570288c640ea72"
+//   };
